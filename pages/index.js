b7c0f68d3ea06e170fe7e6127a6a297c3b0b91ac
Bziomek97 <B.ziomek97@gmail.com>
2021-05-18 23:27:07 +0200

    Add ui framework and auth draft

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,40 @@
+import { Stack, TextField } from '@fluentui/react'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+
+const stackTokens = { childrenGap: 50 };
+const stackStyles = { root: { width: 1200, display: 'flex', justifyContent: 'center' }};
+const columnProps = {
+  tokens: { childrenGap: 15 },
+  styles: { root: { width: 300 } },
+};
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico"/>
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <Stack horizontal tokens={stackTokens} styles={stackStyles} horizontalAlign='Center'>
+        <Stack {...columnProps}>
+          <TextField label="Email"/>
+          <TextField
+            label="Password"
+            type="password"/>
+        </Stack>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        <Stack {...columnProps}>
+          <TextField label="Name"/>
+          <TextField label="Email"/>
+          <TextField
+            label="Password"
+            type="password"/>        
+          <TextField
+            label="Confirm Password"
+            type="password"/>
+        </Stack>
+      </Stack>
     </div>
   )
 }
